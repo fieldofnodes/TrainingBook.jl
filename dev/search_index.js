19d296b0ab4a4c46b92eb3f6a85274e16405867c
Documenter.jl <documenter@juliadocs.github.io>
2024-06-03 23:44:40 +0000

    build based on 106e2db

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TrainingBook","category":"page"},{"location":"#TrainingBook","page":"Home","title":"TrainingBook","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TrainingBook.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TrainingBook]","category":"page"}]
+}
